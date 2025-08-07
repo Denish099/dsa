e35de98418407ad9048e4f70d8a28eb4da45d5bf
Denish099 <gyldenish@gmail.com>
2025-08-07 18:38:59 +0530

    added string

--- string/string.cpp
+++ string/string.cpp
@@ -1,23 +1,23 @@
-// #include <iostream>
-// #include <cstring>
-// using namespace std;
-
-// int main()
-// {
-//     char str[] = "ab";
-//     cout << (str);
-//     return 0;
-// }
-
 #include <iostream>
-#include <string>
+#include <cstring>
 using namespace std;
 
 int main()
 {
-    char fullName[100];
-    cout << "Enter your full name: ";
-    cin.getline(fullName, 100, '&');
-    cout << "Full name: " << fullName << std::endl;
+    char str[] = "ab";
+    cout << (str);
     return 0;
 }
+
+// #include <iostream>
+// #include <string>
+// using namespace std;
+
+// int main()
+// {
+//     char fullName[100];
+//     cout << "Enter your full name: ";
+//     cin.getline(fullName, 100, '&');
+//     cout << "Full name: " << fullName << std::endl;
+//     return 0;
+// }
